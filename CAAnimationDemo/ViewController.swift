a9ef132c2e005168bfb94ad879f6ab1be98ed94a
Roland Tecson <roland_tecson@hotmail.com>
2019-02-19 22:49:50 -0800

    Update to Swift 4

--- CAAnimationDemo/ViewController.swift
+++ CAAnimationDemo/ViewController.swift
@@ -21,25 +21,27 @@ class ViewController: UIViewController {
         // Add as a sublayer to our view's layer
         view.layer.addSublayer(layerOne)
 
-//        // Animate opacity
-//        let opacityAnim = CABasicAnimation(keyPath: "opacity")
-//        opacityAnim.fromValue = 1
-//        opacityAnim.toValue = 0
-//        opacityAnim.duration = 2
-////        opacityAnim.delegate = self
-//        opacityAnim.repeatCount = 2
-//        opacityAnim.autoreverses = true
-//        layerOne.add(opacityAnim, forKey: "opacityAnim")
-//
+        // Animate opacity
+        let opacityAnim = CABasicAnimation(keyPath: "opacity")
+        opacityAnim.fromValue = 1
+        opacityAnim.toValue = 0
+        opacityAnim.duration = 2
+        opacityAnim.delegate = self
+        opacityAnim.repeatCount = 3
+        opacityAnim.autoreverses = true
+//        opacityAnim.speed = 100
+        layerOne.add(opacityAnim, forKey: "opacityAnim")
+
 //        // Animate background color
 //        let colorAnim = CABasicAnimation(keyPath: "backgroundColor")
 //        colorAnim.fromValue = UIColor.blue.cgColor
 //        colorAnim.toValue = UIColor.green.cgColor
 //        colorAnim.duration = 0.75
-//        colorAnim.repeatCount = HUGE  // HUGE == keep repeating
+//        colorAnim.delegate = self
+//        colorAnim.repeatCount = 2  // HUGE == keep repeating
 //        colorAnim.autoreverses = true
 //        layerOne.add(colorAnim, forKey: nil)
-//
+
 //        // Animate y position
 //        let moveAnim = CABasicAnimation(keyPath: "position.y")
 //        moveAnim.fromValue = 100
@@ -48,7 +50,7 @@ class ViewController: UIViewController {
 //        moveAnim.repeatCount = HUGE
 //        moveAnim.autoreverses = true
 //        layerOne.add(moveAnim, forKey: nil)
-//
+
 //        // Animate corner radius
 //        let cornerAnim = CABasicAnimation(keyPath: "cornerRadius")
 //        cornerAnim.fromValue = 0
@@ -57,21 +59,22 @@ class ViewController: UIViewController {
 //        cornerAnim.repeatCount = HUGE
 //        cornerAnim.autoreverses = true
 //        layerOne.add(cornerAnim, forKey: nil)
-//
+
 //        // Create another CALayer sublayer
 //        let layerTwo = CALayer()
 //        layerTwo.frame = CGRect(x: 200, y: 20, width: 150, height: 150)
 //        layerTwo.backgroundColor = UIColor.red.cgColor
 //        view.layer.addSublayer(layerTwo)
-//
-////        // Group the following animations into one CA transaction
-////        // Call completion block when all the animations have completed
-////        // Animations may be of different lengths, completed only when the longest one is finished
-////        CATransaction.begin()
-////        CATransaction.setCompletionBlock {
-////            print("Finished animating layer two")
-////            layerTwo.removeFromSuperlayer()
-////        }
+
+//        // Group the following animations into one CA transaction (everything betweeen begin() and commit()
+//        // Call completion block when all the animations have completed
+//        // Animations may be of different lengths, completed only when the longest one is finished
+//        CATransaction.begin()
+//        CATransaction.setCompletionBlock {
+//            // This is called when all the animations in the transaction has completed
+//            print("Finished animating layer two")
+//            layerTwo.removeFromSuperlayer()
+//        }
 //
 //        // Show use of keyframe animation (define intermediate values and times (between 0 and 1) for each value)
 //        let moveKeysAnim = CAKeyframeAnimation(keyPath: "position")
@@ -79,15 +82,15 @@ class ViewController: UIViewController {
 //                               CGPoint(x: 250, y: 100),
 //                               CGPoint(x:150, y: 300),
 //                               CGPoint(x: 300, y: 200)]
-//        moveKeysAnim.keyTimes = [0, 0.1, 0.75, 1]
+////        moveKeysAnim.keyTimes = [0, 0.1, 0.75, 1]
 //        moveKeysAnim.duration = 3
-////        moveKeysAnim.autoreverses = true
-//        moveKeysAnim.repeatCount = 3
+//        moveKeysAnim.autoreverses = true
+//        moveKeysAnim.repeatCount = 1
 ////        moveKeysAnim.calculationMode = kCAAnimationDiscrete
-////        moveKeysAnim.keyTimes = [0, 0.1, 0.75, 0.95, 1]
-////        moveKeysAnim.calculationMode = kCAAnimationPaced
+//        moveKeysAnim.keyTimes = [0, 0.1, 0.5, 0.8, 1]  // These key times are for discrete animation, one more key time than values
+////        moveKeysAnim.calculationMode = kCAAnimationPaced  // Key times are ignored here
 ////        moveKeysAnim.calculationMode = kCAAnimationCubic
-////        moveKeysAnim.calculationMode = kCAAnimationCubicPaced
+//        moveKeysAnim.calculationMode = CAAnimationCalculationMode.cubicPaced  // Key times are ignored here
 //        layerTwo.add(moveKeysAnim, forKey: nil)
 //
 //        let colorKeysAnim = CAKeyframeAnimation(keyPath: "backgroundColor")
@@ -96,11 +99,11 @@ class ViewController: UIViewController {
 //                                UIColor.green.cgColor,
 //                                UIColor.brown.cgColor]
 //        colorKeysAnim.duration = 1.5
-//        colorKeysAnim.repeatCount = 6 // 7
+//        colorKeysAnim.repeatCount = 6
 //        layerTwo.add(colorKeysAnim, forKey: nil)
 //
-////        CATransaction.commit()
-//
+//        CATransaction.commit()
+
 //        let layerThree  = CALayer()
 //        layerThree.backgroundColor = UIColor.green.cgColor
 //        layerThree.frame = CGRect(x: 100, y: 100, width: 100, height: 150)
@@ -115,15 +118,14 @@ class ViewController: UIViewController {
 //        pathMoveAnim.path = path
 //        pathMoveAnim.duration = 4
 //        pathMoveAnim.repeatCount = HUGE
-//        pathMoveAnim.rotationMode = kCAAnimationRotateAuto
+//        pathMoveAnim.rotationMode = CAAnimationRotationMode.rotateAuto
 //        layerThree.add(pathMoveAnim, forKey: nil)
-
     }
 }
 
-//extension ViewController : CAAnimationDelegate {
-//    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-//        print("Animation done \(anim)")
-//    }
-//}
+extension ViewController : CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        print("Animation done \(anim)")
+    }
+}
 
